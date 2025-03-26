64e77c9d50ad20508cbf04ae39c1904eeccbe2fb
Oleksandr274 <s00274217@atu.ie>
2025-03-26 10:45:47 +0000

    ca2 part1 done

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { CarApiService } from '../../services/car-api.service';
+import { ICar } from '../../interfaces/car';
+
+@Component({
+  selector: 'app-car',
+  imports: [],
+  templateUrl: './car.component.html',
+  styleUrl: './car.component.css'
+})
+export class CarComponent {
+  @Input() carData?:ICar | any
+
+  carImageWidth: number = 300
+
+  constructor(private _carAPIService: CarApiService){}
+
+  deleteCar(carId:string) { 
+    this._carAPIService.delCarDetails(carId).subscribe(result =>
+      { 
+        console.log(result);
+      });
+  }
+}
